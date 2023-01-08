5f34cd43c1f79380d9b8111fc5460779626b9363
dhananjaygola <dhananjay.181018@gmail.com>
2023-01-08 21:42:19 +0530

    second commit

---
+++ godrej/thanks.php
@@ -0,0 +1,296 @@
+<html class=" js flexbox canvas canvastext webgl no-touch geolocation postmessage websqldatabase indexeddb hashchange history draganddrop websockets rgba hsla multiplebgs backgroundsize borderimage borderradius boxshadow textshadow opacity cssanimations csscolumns cssgradients cssreflections csstransforms csstransforms3d csstransitions fontface generatedcontent video audio localstorage sessionstorage webworkers no-applicationcache svg inlinesvg smil svgclippaths" lang="zxx"><head><meta http-equiv="content-type" content="text/html;charset=UTF-8"><!-- /Added by HTTrack -->
+
+<meta charset="utf-8">
+<meta http-equiv="x-ua-compatible" content="ie=edge">
+<title>Risland | Sky Mansion</title>
+<meta name="robots" content="noindex, follow">
+<meta name="description" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+
+<link rel="stylesheet" href="css/font-icons.css">
+
+<link rel="stylesheet" href="css/plugins.css">
+
+<link rel="stylesheet" href="css/style.css">
+
+<link rel="stylesheet" href="css/responsive.css">
+<style>
+       /*.logo_bg{
+            border-radius: 80px;
+            background-color: #fff;
+            padding: 5px 20px;
+       }*/
+        .gradient-color-2{background: none;}
+        #scrollUp{display:none !important;}
+        
+        .iti__country-list {
+            width: 100%;
+            min-width: 300px;
+        }
+        #lightbox-container-image img { width:auto; max-height:520px; }
+        .iti--allow-dropdown {
+            width: 100%;
+            margin-bottom: 20px;
+        }
+
+        .iti--allow-dropdown span {
+            color: #000;
+        }
+
+        .iti--allow-dropdown input,
+        .iti--allow-dropdown input[type="tel"],
+        .iti--allow-dropdown input[type="text"],
+        .iti--separate-dial-code input,
+        .iti--separate-dial-code input[type="tel"],
+        .iti--separate-dial-code input[type="text"] {
+            padding-left: 52px !important;
+        }
+
+        .alert-danger {
+            display: none;
+        }
+
+        .alert-danger span {
+            color: #000;
+        }
+        @media (max-width: 600px){
+            .logo_bg{max-width: 35px;}
+            .footer-top-area .row .col-xl-4:last-child{
+                position: relative;
+            }
+            .site-logo {
+                width: 119px;
+            }
+            .gradient-color-2{
+                
+            }
+            .modal-dialog {
+                 margin-top: 20px; 
+            }
+            .CTA{
+                display:flex !important;
+            }
+        }
+            .CTA{
+                display: none;
+                position: fixed;
+                z-index: 999;
+                bottom: 0;
+                width:100%;
+                
+            }
+            .CTA .whatsapp{
+                background-color:#25d366;
+                color:#fff;
+                padding:6px 30px;
+                flex-basis:50%;
+                font-size: 16px;
+                align-items: center;
+                justify-content: center;
+                display: flex;
+            }
+            .CTA .callnow{
+                background-color: #8b6a2f;
+                color:#fff;
+                font-size: 16px;
+                padding:6px 30px;
+                flex-basis:50%;
+                justify-content: center;
+                display: flex;
+                align-items: center;
+            }
+            .CTA .callnow a:hover{
+                color:#fff;
+            }
+            .CTA .whatsapp a:hover{
+                color:#fff;
+            }
+            
+        </style>
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js "></script>
+ <!-- Global site tag (gtag.js) - Google Ads: 685964924 -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-685964924"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-685964924');
+</script>
+
+<!-- Event snippet for Risland_im_fp conversion page -->
+<script>
+  gtag('event', 'conversion', {'send_to': 'AW-685964924/E2uJCIi31IcDEPz8i8cC'});
+</script>
+
+</head>
+<body>
+
+
+
+<div class="body-wrapper">
+
+<header class="ltn__header-area ltn__header-5 ltn__header-logo-and-mobile-menu-in-mobile ltn__header-logo-and-mobile-menu ltn__header-transparent gradient-color-2" style="background-color:#000!important;">
+
+<div class="ltn__header-middle-area ltn__header-sticky ltn__sticky-bg-black">
+<div class="container">
+<div class="row" style="align-items: flex-start;">
+<div class="col">
+<div class="site-logo-wrap">
+<div class="site-logo">
+<a href="#"><img class="logo_bg" src="https://www.fairpockets.com/risland-sky-mansion/img/logo-w.png" alt="Logo"></a>
+</div>
+</div>
+</div>
+<div class="col header-menu-column menu-color-white">
+<div class="header-menu d-none d-xl-block">
+<nav>
+<div class="ltn__main-menu">
+<ul>
+<li><a href="#About_us">About Us</a></li>
+<li><a href="#Apartments_Plan">Apartments Plan</a></li>
+<li><a href="#Price_List">Price List</a></li>
+<li><a href="#Aminities">Aminities</a></li>
+<li><a href="#Gallery">Gallery</a></li>
+<li><a href="#Testimonial">Testimonial</a></li>
+<li class="special-link">
+<a href="tel:+919818541202">Call : +91-9818541202</a>
+</li>
+</ul>
+</div>
+</nav>
+</div>
+</div>
+<div class="col--- ltn__header-options ltn__header-options-2 ">
+
+<div class="mobile-menu-toggle d-xl-none">
+<a href="#ltn__utilize-mobile-menu" class="ltn__utilize-toggle">
+<svg viewBox="0 0 800 600">
+<path d="M300,220 C300,220 520,220 540,220 C740,220 640,540 520,420 C440,340 300,200 300,200" id="top"></path>
+<path d="M300,320 L540,320" id="middle"></path>
+<path d="M300,210 C300,210 520,210 540,210 C740,210 640,530 520,410 C440,330 300,190 300,190" id="bottom" transform="translate(480, 320) scale(1, -1) translate(-480, -318) "></path>
+</svg>
+</a>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+</header>
+
+
+
+ 
+<div class="ltn__utilize-overlay"></div>
+
+<div class="ltn__slider-area ltn__slider-4 position-relative ltn__primary-bg">
+
+</div>
+
+
+
+
+
+
+
+
+<div id="Price_List" class="ltn__about-us-area pt-60 pb-100 ">
+<div class="container">
+<div class="row">
+<div class="col-lg-12 align-self-center">
+<div class="thankyou" style="padding: 100px 100px;text-align: center;">
+            <img src="https://fairpockets.com/ace-divino/thanks.jpg" class="mb-30">
+            <h3>Your message has been sent successfully </h3>
+            <a href="index.php" class="btn btn-success">Back To Home</a>
+          </div>
+</div>
+</div>
+</div>
+</div>
+
+
+
+<footer class="ltn__footer-area  ">
+
+<div class="ltn__copyright-area ltn__copyright-2 section-bg-7  plr--5">
+<div class="container-fluid ltn__border-top-2">
+<div class="row">
+<div class="col-md-6 col-12">
+<div class="ltn__copyright-design clearfix">
+<p>All Rights Reserved @ Risland <span class="current-year">2021</span></p>
+</div>
+</div>
+<div class="col-md-6 col-12 align-self-center">
+<div class="ltn__copyright-menu text-end">
+<ul>
+<li><a href="#">Terms &amp; Conditions</a></li>
+<li><a href="#">Claim</a></li>
+<li><a href="#">Privacy &amp; Policy</a></li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+</div>
+</footer>
+
+<div class="CTA">
+<div class="whatsapp">
+<a href="https://api.whatsapp.com/send?phone=+919818541202&amp;text=Hi%2C%20I%27m%20interested%20in%20Ace%20Divino.%20Please%20share%20details%20about%20this%20project."><i class="fab fa-whatsapp"></i> Whatsapp</a>
+</div>
+<div class="callnow">
+<a href="tel:+919818541202"><i class="icon-call"></i> Call Now</a>
+</div>
+</div>
+
+
+<div class="ltn__modal-area ltn__add-to-cart-modal-area">
+<div class="modal fade" id="liton_wishlist_modal" tabindex="-1">
+<div class="modal-dialog modal-md" role="document">
+<div class="modal-content">
+<div class="modal-header">
+<button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+<span aria-hidden="true">×</span>
+</button>
+</div>
+<div class="modal-body">
+<div class="ltn__quick-view-modal-inner">
+<div class="modal-product-item">
+<div class="row">
+<div class="col-12">
+<form action="sendmail.php" method="POST">
+<h3 class="animated fadeIn">Book Your Dream Home</h3>
+<input type="text" name="name" placeholder="Name :">
+<input type="text" name="mobile" placeholder="Phone :">
+<input type="text" name="email" placeholder="Email :">
+
+
+<div class="alert alert-danger error_display_im_3" role="alert">
+<span id="error_text_im_3"></span>
+</div>
+<button class="btn theme-btn-1 btn-effect-3 text-uppercase" id="f3s" name="send" type="submit">Book your flat Now</button>
+</form>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+</div>
+
+
+<script data-cfasync="false" src="../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
+<script src="js/plugins.js"></script>
+
+<script src="js/main.js"></script>
+
+
+
+<a id="scrollUp" href="#top" style="position: fixed; z-index: 2147483647; display: none;"><i class="fa fa-angle-up"></i></a></body></html>
